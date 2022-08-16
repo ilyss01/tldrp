13ed4354bf617a2a1843ad4af841983e6e0e69b6
ilyss01 <ilya.romanuck2014@yandex.ru>
2022-08-16 20:31:32 +0300

    Added todos and comments in main.py. Tweaked test2 for multiline args

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import argparse
 
+# TODO: print multiline args
+# TODO: module system for multiple programming languages
+# TODO: make it readable and more or less good project
+# (use env, make __main__, make requirements and so on)
+
 # parse arguments and make --help page
 parser = argparse.ArgumentParser()
 parser.add_argument('parsed_file',help='Name of the file', type=str)
@@ -9,12 +14,14 @@ with open(args.parsed_file, 'r') as file:
     for line in file:
         # skipping empty lines
         if line == '\n' or line.isspace(): file.read(1)
-        # functions not in classes
+        # functions that are not in classes
         elif line[:3] == 'def':
             print()
             print(line)
+        # clases themselves
         elif line.split()[0] == 'class':
             print(line, end='')
+        # class methods
         elif line.split()[0] == 'def':
             print(line, end='')
 
