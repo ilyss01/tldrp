a33e4a37e3ea856fabb9f74ca78e7357241cdd99
ilyss01 <ilya.romanuck2014@yandex.ru>
2022-08-20 09:30:06 +0300

    Added rust support, fixed some typos

--- main.py
+++ main.py
@@ -2,31 +2,40 @@ import argparse
 
 # TODO: print multiline args
 # TODO: module system for multiple programming languages
-# TODO: make it readable and more or less good project
-# (use env, make __main__, make requirements and so on)
 
-# parse arguments and make --help page
+# parse arguments 
 parser = argparse.ArgumentParser()
-parser.add_argument('parsed_file',help='Name of the file', type=str)
+parser.add_argument('parsed_file', help='Name of the file', type=str)
 args = parser.parse_args()
 
-# file extension
-#print(args.parsed_file.split('.')[1])
+# file extension defenition
+if args.parsed_file.split('.')[1] == 'py':
+    function_length = 3
+    class_declaration = 'class'
+    function_declaration = 'def'
+    pub_function = 'def'
+elif args.parsed_file.split('.')[1] == 'rs':
+    function_lengh = 2
+    class_declaration = 'impl'
+    function_declaration = 'fn'
+    pub_function = 'pub'
+else:
+    print('Not supported file extension')
+    return 0
 
+# parsing file and printing functions and classes
 with open(args.parsed_file, 'r') as file:
     for line in file:
         # skipping empty lines
         if line == '\n' or line.isspace(): file.read(1)
         # functions that are not in classes
-        elif line[:3] == 'def':
+        elif line[:function_length] == function_declaration:
             print()
             print(line)
         # clases themselves
-        elif line.split()[0] == 'class':
+        elif line.split()[0] == class_declaration:
             print(line, end='')
         # class methods
-        elif line.split()[0] == 'def':
+        elif line.split()[0] == function_declaration or line.split()[0] ==pub_function:
             print(line, end='')
-
-
 
