769567e7057fb3c3900cdcb3da2937f726f53311
ilyss01 <ilya.romanuck2014@yandex.ru>
2022-08-23 14:06:46 +0300

    Code improvements. Added function for file extention detection

--- src/main.py
+++ src/main.py
@@ -11,39 +11,54 @@ def multiline_args(line):
         if ')' in line:
             closing_brackets += 1
 
-# made main function for stopping the program if filetype is not supported
+# kind of modularity, to add new programming language you just need to add new if statement
+def match_file_type(extension):
+    # 6 variables 
+    """Returns tuple of func_lengh, func_decl, pub_func, priv_func, class_decl, another_class_decl"""
+    if extension == 'py':
+        # python 
+        return (3, 'def', 'def', 'def', 'class', 'class')
+    elif extension == 'rs':
+        # rust 
+        return (2, 'fn', 'pub', 'fn', 'trait', 'impl')
+    else:
+        print('File type is not supported')
+        exit()
+
 def main():
     # parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('parsed_file', help='Name of the file', type=str)
+    parser = argparse.ArgumentParser(description='Prints out functions and classes of the source code')
+    parser.add_argument('parsed_file', help='Source code of the program',  default=None)
     args = parser.parse_args()
 
-    # file extension defenition
-    if args.parsed_file.split('.')[1] == 'py':
-        function_length = 3
-        class_declaration = 'class'
-        function_declaration = 'def'
-        pub_function = 'def'
-    elif args.parsed_file.split('.')[1] == 'rs':
-        function_lengh = 2
-        class_declaration = 'impl'
-        function_declaration = 'fn'
-        pub_function = 'pub'
-    else:
+    print(args)
+    
+    # check for arguments
+    if args.parsed_file == None:
+        exit()
+
+    # filetype variables
+    try:
+        func_length, func_decl, pub_func, private_func, class_decl, \
+    another_class_decl = match_file_type(args.parsed_file.split('.')[1])
+    except IndexError:
         print('Not supported file extension')
-        return 0
+        exit()
 
     # parsing file and printing functions and classes
     with open(args.parsed_file, 'r') as file:
         for line in file:
             # skipping empty lines
             if line == '\n' or line.isspace(): file.read(1)
-            # functions that are not in classes
-            elif line[:function_length] == function_declaration:
+            # functions declaration
+            elif line[:func_length] == func_decl:
                 print('\n', line)
-            # clases themselves
-            elif line.split()[0] == class_declaration:
+            # clases declaration
+            elif line.split()[0] == class_decl or \
+                 line.split()[0] == another_class_decl:
                 print(line, end='')
-            # class methods
-            elif line.split()[0] == function_declaration or line.split()[0] ==pub_function:
+            # class methods (public and private ones)
+            elif line.split()[0] == func_decl or \
+                 line.split()[0] == pub_func or \
+                 line.split()[0] == private_func:
                 print(line, end='')
