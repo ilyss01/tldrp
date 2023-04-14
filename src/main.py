052c4a7b499f175caf102b491b98554bfb6e0c04
Ilya Romanuk <ilya.romanuck2014@yandex.ru>
2023-04-14 20:25:13 +0300

    following pep8, changed some functions and stuff

--- src/main.py
+++ src/main.py
@@ -1,44 +1,57 @@
 import argparse
 
+
 # TODO: print multiline args
 def multiline_args(line):
     pass
+
 
 # TODO: add module parse
 def parse_modules(file):
     pass
 
-# kind of modularity, to add new programming language you just need to add new if statement
-def match_file_type(extension):
-    # 6 variables 
-    """Returns tuple of func_decl, func_pub, func_private, class_decl, another_class_decl"""
-    if extension == 'py':
-        # python language
-        return ('def', 'def', 'def', 'class', 'class')
-    elif extension == 'rs':
-        # rust language
-        return ('fn', 'pub', 'fn', 'trait', 'impl')
+
+def get_lang_keywords(extension):
+    #
+    # define filetype by extension, should return tuple of keywords,
+    # not something abstract
+    #
+    """Returns tuple of keywords"""
+    # python lang
+    if extension in ('py', 'pyi', 'pyc', 'pyd', 'pyw', 'pyz', 'pyo'):
+        return ('def', 'class')
+    # rust lang
+    elif extension in ('rs', 'rlib'):
+        return ('fn', 'pub', 'trait', 'impl')
+    # go lang
+    elif extension in ('go'):
+        return ()
+    # TODO: c lang
     else:
-        # break for not supported filetypes 
+        # break for not supported filetypes
         print('File type is not supported')
         exit()
 
+
 def main():
     # parse arguments
-    parser = argparse.ArgumentParser(description='Prints out functions and classes of the source code')
-    parser.add_argument('parsed_file', help='Source code of the program', default=None)
+    parser = argparse.ArgumentParser(
+            description='Prints out functions and classes of the source code'
+    )
+    parser.add_argument('parsed_file',
+                        help='Source code of the program',
+                        default=None)
     args = parser.parse_args()
-    
+
     # check for any args
-    if args.parsed_file == None:
+    if args.parsed_file is None:
         exit()
 
     # filetype variables
     try:
-        func_decl, func_pub, func_private, class_decl, \
-        another_class_decl = match_file_type(args.parsed_file.split('.')[1])
+        lang_keywords = get_lang_keywords(args.parsed_file.split('.')[1])
     except IndexError:
-        # catch for files with no dot in their name
+        # catch for files w/o dot in their name
         print('File type is not supported')
         exit()
 
@@ -46,13 +59,9 @@ def main():
     with open(args.parsed_file, 'r') as file:
         for line in file:
             # skipping empty lines
-            if line == '\n' or line.isspace(): file.read(1)
-            # clases declaration
-            elif line.split()[0] == class_decl or \
-                 line.split()[0] == another_class_decl:
+            if line == '\n' or line.isspace():
+                file.read(1)
+            # found keyword
+            elif line.split()[0] in lang_keywords:
+                # case for multiline args
                 print(line, end='')
-            # class methods and functions
-            elif line.split()[0] == func_decl or \
-                 line.split()[0] == func_pub or \
-                 line.split()[0] == func_private:
-                print(line)
